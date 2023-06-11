a8b9b2286b97733ee67c51062a28e511f6495516
Mahavir Patel <patelmahavir2003@outlook.com>
2023-06-11 19:35:45 +0530

    Added Menu Opening Animation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,77 +1,49 @@
 import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+import { useState, useEffect, useCallback } from "react";
+import Menu from "./Menu";
+import { gsap } from "gsap";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = useCallback(() => {
+    setIsMenuOpen((prevIsMenuOpen) => !prevIsMenuOpen);
+  }, []);
+
+  useEffect(() => {
+    let tl = gsap.timeline();
+
+    if (isMenuOpen) {
+      tl.to(".menu", {
+        duration: 0.7,
+        height: "100vh",
+        ease: "expo.inOut",
+        position: "fixed",
+      })
+    } else {
+      tl.to(".menu", {
+        duration: 0.7,
+        height: "0vh",
+        ease: "expo.inOut",
+        position: "relative",
+      });
+    }
+  }, [isMenuOpen]);
+
   return (
-    <header className="w-full h-auto absolute top-0 left-0">
+    <header className="w-full h-auto  top-0 left-0">
       <nav className="bg-black w-full h-auto flex justify-center items-start relative">
-        <div className="navbar z-10 absolute top-0 flex justify-between items-center w-11/12 sm:w-9/12 pt-8">
+        <div className="navbar z-10 absolute top-0 flex justify-between items-center w-11/12 sm:w-9/12 pt-5 lg:pt-8">
           <span className="logo">
-            <p className=" font-primary text-xl sm:text-2xl font-semibold tracking-widest">
+            <p className=" font-primary text-sm sm:text-2xl font-semibold tracking-widest">
               CLIENET.
             </p>
           </span>
-          <span className="menu-icon">
-            <Image
-              src="./svg/Menu_Arrow.svg"
-              alt="menu"
-              width={50}
-              height={50}
-              className="Menu-icon 2xl:w-16 w-10 sm:w-12 "
-            />
+          <span className="Menu-icon">
+            <Image src="/svg/Menu_Arrow.svg" alt="menu" width={40} height={40} className="w-10 sm:w-14 hover:cursor-pointer" onClick={toggleMenu} />
           </span>
         </div>
-        <div className="menu w-full h-screen bg-bright-yellow overflow-hidden flex justify-center">
-          <div className="menu-container mt-10 md:mt-0 xl:justify-around xl:w-9/12 xl:-mt-10  w-9/12 h-full flex justify-center gap-y-14 lg:items-center lg:w-11/12 lg:gap-x-16 lg:mt-5 items-start flex-col lg:flex-row">
-            <div className="menu-items select-none">
-                <span><h1 className=" font-secondary font-light text-lg md:text-2xl md:mb-8 lg:text-xl 2xl:font-normal 2xl:mb-10
-               2xl:text-2xl mb-4 select-none sm:mb-6">Menu</h1></span>
-                <span className="font-secondary text-xl md:text-4xl lg:text-3xl md:font-medium xl:text-4xl 2xl:text-5xl
-             tracking-widest font-semibold">
-                    <ul className="flex flex-col gap-y-2 sm:gap-y-4 2xl:gap-y-8">
-                        <li><Link className="link after:mt-3" href="/">Case Studies</Link></li>
-                        <li><Link className="link after:mt-3" href="/">Approach</Link></li>
-                        <li><Link className="link after:mt-3" href="/">Services</Link></li>
-                        <li><Link className="link after:mt-3" href="/">About Us</Link></li>
-                    </ul>
-                </span>
-            </div>
-            <div className="menu-contact">
-                <span><h1 className="font-secondary font-light text-lg font mb-4 select-none sm:mb-6 md:text-2xl lg:text-xl 2xl:font-normal 2xl:mb-10
-               2xl:text-2xl">Contact</h1></span>
-                <div className="grid grid-cols-2 gap-2 select-none gap-x-6 sm:gap-8 sm:gap-x-16 2xl:gap-20">
-                    <div>
-                        <h1 className="font-primary pb-4 md:text-2xl lg:text-xl text-xl select-none font-semibold 2xl:text-2xl">Email</h1>
-                        <ul className="font-secondary font-light md:tracking-wider xl:tracking-wider md:font-normal text-lg flex flex-col gap-y-2 2xl:text-xl 2xl:gap-y-4">
-                            <li><Link href="/" className="link after:mt-1">Get in touch with us</Link></li>
-                            <li><Link href="/" className="link after:mt-1">Get a free audit</Link></li>
-                        </ul>
-                    </div>
-                    <div className="hidden sm:block">
-                    <h1 className="font-primary pb-4 lg:text-xl text-2xl select-none font-semibold 2xl:text-2xl">Headquarter</h1>
-                        <ul className="font-secondary xl:tracking-wider font-light md:font-normal tracking-wider text-lg flex flex-col gap-y-2 2xl:text-xl 2xl:gap-y-4">
-                            <li><Link href="/">123, Main Street</Link></li>
-                            <li><Link href="/">Civil Lines, Delhi</Link></li>
-                            <li><Link href="/"> Delhi-110001, India</Link></li>
-                        </ul>
-                    </div>
-                    <div className="hidden sm:block">
-                    <h1 className="font-primary lg:text-xl  pb-4 text-2xl select-none font-semibold 2xl:text-2xl">Phone</h1>
-                        <ul className="font-secondary  xl:tracking-wider font-light md:font-normal tracking-wider text-lg  flex flex-col gap-y-2 2xl:text-xl 2xl:gap-y-4">
-                        <li><Link href="/" className="link after:mt-1">+91 &nbsp; 3487834341</Link></li>
-                        </ul>
-                    </div>
-                    <div>
-                    <h1 className="font-primary xl:tracking-wider pb-4 md:text-2xl text-xl select-none font-semibold lg:text-xl 2xl:text-2xl">Legal</h1>
-                        <ul className="font-secondary md:tracking-wider md:font-normal font-light text-lg flex flex-col gap-y-2 2xl:text-xl 2xl:gap-y-4">
-                            <li><Link href="/">Privacy & Cookies</Link></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-          </div>
-        </div>
+        <Menu />
       </nav>
     </header>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import Navbar from '@/components/Navbar'
-import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
      <Navbar />
+     <div className=' bg-white'>Welcome To Clienet</div>
     </>
   )
 }
